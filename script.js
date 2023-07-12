c79f9d55e3950f4ad16333b87e89cd4ac365748f
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-07-12 10:36:59 -0500

    Update
    Fix HTML bug

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 /* eslint-disable */
 const form = document.querySelector('form');
 const bookList = document.getElementById('bookList');
-const menua = document.querySelector('.menua');
-const menub = document.querySelector('.menub');
-const menuc = document.querySelector('.menuc');
-const main = document.querySelector('.list');
-const add = document.querySelector('.add_new');
-const contact = document.querySelector('.contact');
 const books = [];
 
 class Abooks {
@@ -59,23 +53,8 @@ bookList.addEventListener('click', (event) => {
     const { index } = event.target.dataset;
     books.splice(index, 1);
     localStorage.setItem('books', JSON.stringify(books));
+    const Abooksa = new Abooks(books);
     Abooksa.displayBooks();
   }
 });
 Abooksa.displayBooks();
-
-menua.addEventListener('click', () => {
-  main.classList.toggle('hide');
-  add.classList.add('hide');
-  contact.classList.add('hide');
-});
-menub.addEventListener('click', () => {
-  add.classList.toggle('hide');
-  main.classList.add('hide');
-  contact.classList.add('hide');
-});
-menuc.addEventListener('click', () => {
-  contact.classList.toggle('hide');
-  main.classList.add('hide');
-  add.classList.add('hide');
-});
